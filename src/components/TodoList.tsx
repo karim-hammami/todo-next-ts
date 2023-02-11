03639a934662d4addb98914d9d27486ce7dc3cac
karim-hammami <hammamik790@gmail.com>
2023-02-11 17:25:40 +0100

    proper styling for list all todos component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -23,18 +23,20 @@ export const TodoList = () => {
         return (
             <Center flex="true" alignItems="center" justifyContent="center">
                 <Flex alignItems="center" justify="center" direction="column" >
-                        {res.map((item) => {
-                            return (
-                                <>
-                                <Box borderWidth="1px" borderRadius="xl" m="5" p="5" display="flex" justifyContent="space-evenly" gap="10">
+                    {res.map((item) => {
+                        return (
+                            <>
+                                <Box borderWidth="1px" borderRadius="xl" m="5" p="5" display="flex" justifyContent="space-between" gap="10" w={900}>
                                     <Tag key={item.id} variant="solid" colorScheme="teal" fontSize="3xl">{item.status}</Tag>
                                     <Text key={item.id} fontSize="3xl">{item.title}</Text>
-                                    <Button leftIcon={<EditIcon />} colorScheme="teal">Edit</Button>
-                                    <Button leftIcon={<DeleteIcon />} colorScheme="red">Delete</Button>
+                                    <Box >
+                                    <Button mx="5" leftIcon={<EditIcon />} colorScheme="teal">Edit</Button>
+                                    <Button mx="5" leftIcon={<DeleteIcon />} colorScheme="red">Delete</Button>
+                                    </Box>
                                 </Box>
-                                </>
-                            )
-                        })}
+                            </>
+                        )
+                    })}
 
                 </Flex>
             </Center>
